31d0a8676195110bb9a9ec23c84d04577e041f1f
Niclas Kjäll-Ohlsson <niclasko@gmail.com>
2021-12-03 20:34:48 +0100

    Update ExpressionParser.js

--- js/ExpressionParser.js
+++ js/ExpressionParser.js
@@ -1,3 +1,22 @@
+/*
+* ExpressionParser.js infix expression parser for Javascript
+* Copyright (c) 2021 "Niclas Kjäll-Ohlsson"
+* 
+* This file is part of ExpressionParser.js.
+* 
+* ExpressionParser.js is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ExpressionParser.js is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ExpressionParser.js.  If not, see <https://www.gnu.org/licenses/>.
+*/
 function ExpressionParser() {
 	
 	var expression = null;
