01d7bde5c443676d001fe327fca5bf6620d5e81a
Niclas Kjall-Ohlsson <niclask@microsoft.com>
2021-12-05 13:23:18 +0100

    Runners for nix and Windows

--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
-Start-Process "http://localhost:8000/"
+python -m webbrowser http://localhost:8000/
 python -m http.server 8000
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 -m webbrowser http://localhost:8000/
+python -m http.server 8000
